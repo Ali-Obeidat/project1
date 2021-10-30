a76cce49a2976768dfc185606ccfe97406a6d90e
Ali Obeidat <90185197+Ali-Obeidat@users.noreply.github.com>
2021-10-31 00:01:52 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 let section = document.querySelector(".skills ");
 let spans = document.querySelectorAll(".progress-bar");
 
-window.onscroll = () => {
+let nums = document.querySelectorAll(".count-box span");
+let factsSection = document.querySelector("#facts");
+let started = false;
+
+window.onscroll = function () {
+  if (window.scrollY >= factsSection.offsetTop - 400) {
+    if (!started) {
+      nums.forEach((ele) => startCount(ele));
+    }
+    started = true;
+  }
   if (window.scrollY >= section.offsetTop - 400) {
     spans.forEach((e) => {
       e.style.width = e.dataset.width;
     });
   }
 };
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 500 / goal);
+}
